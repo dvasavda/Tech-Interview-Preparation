3bd51cda4ace40c4b0d72f1b0da4822d0293d05b
Dharak Vasavda <dharak@dharaks-mbp.lan>
2020-04-04 20:24:05 -0700

    Updated practice

--- LeetCode - 30 Day Challenge/SingleNumber.py
+++ LeetCode - 30 Day Challenge/SingleNumber.py
@@ -1,4 +1,6 @@
 '''
+Day 1
+
 Single Number
 Given a non-empty array of integers, every element appears twice except for one. Find that single one.
 
---
+++ Week 1 - Arrays and Strings/2. ReverseVowels.py
@@ -0,0 +1,31 @@
+'''
+345. Reverse Vowels of a String
+Easy
+
+
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+
+Input: "hello"
+Output: "holle"
+Example 2:
+
+Input: "leetcode"
+Output: "leotcede"
+Note:
+The vowels does not include the letter "y".
+'''
+
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        #
+
+        vowels = ('aeiouAEIOU')
+
+
+
